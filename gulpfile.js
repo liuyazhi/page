08cfbf4931464e68c0dd221bc318806ca0fc97c3
Your Name <you@example.com>
2017-05-24 20:33:20 +0800

    page

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,10 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');//js压缩
 const cleanCSS = require('gulp-clean-css');//css的压缩
+const webserver = require('gulp-webserver');//web服务热启动
 //js压缩
 gulp.task('js', function () {
     gulp.src('src/js/**/*.js')
-        .pipe(uglify())
         .pipe(gulp.dest('./build/js'));
 });
 //css
@@ -24,3 +24,17 @@ gulp.task('html', function () {
 });
 //执行全部
 gulp.task("build", ["js", "css", "html"]);
+//启动服务
+gulp.task("webserver", ["build"], function () {
+    gulp.watch("./src/css/*.ss", ["css"]);
+    gulp.watch("./src/html/**/*.html", ["html"]);
+    setTimeout(function () {
+        gulp.src('./build')
+            .pipe(webserver({
+                livereload: true,
+                directoryListing: true,
+                open: "/html/index.html"
+            }));
+    }, 1000);
+
+});
